4599586b550560f62d1aeb8dc6fee316559252d7
lin_wsl <hao.lin0728@gmail.com>
2024-07-02 12:11:13 -0400

    updated MainNavBar.jsx layout, added more navbar css

--- react-jobly-fe/src/MainNavBar.jsx
+++ react-jobly-fe/src/MainNavBar.jsx
@@ -1,57 +1,86 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useContext, useState } from "react";
 import {
+  Collapse,
   Navbar,
+  NavbarToggler,
+  NavbarBrand,
   Nav,
   NavItem,
-  NavbarBrand,
-  NavLink as BSNavLink
+  NavLink as BSNavLink,
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import "./MainNavBar.css";
+import { UserContext } from "./UserContextProvider";
 
-const MainNavBar = ({ user = null }) => {
+const MainNavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpenState = () => setIsOpen(!isOpen);
+  const { currentUser: user } = useContext(UserContext);
   const userLink = user ? (
     <>
       <NavItem>
-        <BSNavLink tag={Link} to={`/users/${user.username}`}>{user.username}</BSNavLink>
+        <BSNavLink tag={Link} to={`/users/${user.username}`}>
+          {user.username}
+        </BSNavLink>
       </NavItem>
       <NavItem>
-        <BSNavLink tag={Link} to="/logout">Logout</BSNavLink>
+        <BSNavLink tag={Link} to="/logout">
+          Logout
+        </BSNavLink>
       </NavItem>
     </>
   ) : (
     <>
       <NavItem>
-        <BSNavLink tag={Link} to="/signup">Sign up</BSNavLink>
+        <BSNavLink tag={Link} to="/signup">
+          Sign up
+        </BSNavLink>
       </NavItem>
       <NavItem>
-        <BSNavLink tag={Link} to="/login">Log In</BSNavLink>
+        <BSNavLink tag={Link} to="/login">
+          Log In
+        </BSNavLink>
       </NavItem>
     </>
   );
 
   return (
-    <Navbar color="light" light expand="md" className="mb-4">
-      <NavbarBrand tag={Link} to="/">Jobly</NavbarBrand>
-      <Nav className="mr-auto" navbar>
-        <NavItem>
-          <BSNavLink tag={Link} to="/users">Users</BSNavLink>
-        </NavItem>
-        <NavItem>
-          <BSNavLink tag={Link} to="/companies">Companies</BSNavLink>
-        </NavItem>
-        <NavItem>
-          <BSNavLink tag={Link} to="/jobs">Jobs</BSNavLink>
-        </NavItem>
-        <NavItem>
-          <BSNavLink tag={Link} to="/submit">Submit A Job</BSNavLink>
-        </NavItem>
-        {userLink}
-      </Nav>
+    <Navbar color="light" light expand="sm" className="mb-4">
+      <NavbarBrand tag={Link} to="/">
+        Jobly
+      </NavbarBrand>
+      <NavbarToggler onClick={toggleOpenState} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="me-auto" navbar>
+          <NavItem>
+            <BSNavLink tag={Link} to="/users">
+              Users
+            </BSNavLink>
+          </NavItem>
+          <NavItem>
+            <BSNavLink tag={Link} to="/companies">
+              Companies
+            </BSNavLink>
+          </NavItem>
+          <NavItem>
+            <BSNavLink tag={Link} to="/jobs">
+              Jobs
+            </BSNavLink>
+          </NavItem>
+          <NavItem>
+            <BSNavLink tag={Link} to="/submit">
+              Submit A Job
+            </BSNavLink>
+          </NavItem>
+        </Nav>
+        <Nav className="ms-auto" navbar>
+          {userLink}
+        </Nav>
+      </Collapse>
     </Navbar>
   );
-}
+};
 
 export default MainNavBar;
