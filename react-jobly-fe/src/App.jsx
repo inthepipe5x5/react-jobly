fd81b0eb3ea76beab47d57d859be88c9b1336247
lin_wsl <hao.lin0728@gmail.com>
2024-07-05 22:35:08 -0400

    App.jsx component classnames adjusted (bg, container min-vh/vw)

--- react-jobly-fe/src/App.jsx
+++ react-jobly-fe/src/App.jsx
@@ -1,7 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useContext } from "react";
-import { Container } from "reactstrap";
-
+import {
+  Tooltip,
+  ButtonToolbar,
+  Container,
+  Row,
+  Col,
+  Button,
+  Badge,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 import JoblyApi from "./api";
 import AppRoutes from "./AppRoutes";
 import MainNavBar from "./MainNavBar";
@@ -13,7 +21,8 @@ import FlashMessage from "./FlashMessage";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
-  const { flashMessage, showFlashMessage, DismissFlashMessage } = useContext(FlashMessageContext);
+  const { flashMessage, showFlashMessage, DismissFlashMessage } =
+    useContext(FlashMessageContext);
   const user = useContext(UserContext);
   const [currentUser, setCurrentUser] = useState(user);
 
@@ -24,24 +33,28 @@ function App() {
   }
 
   return (
-      <div className="App">
-        <UserContextProvider>
-          <MainNavBar user={currentUser} />
-          {flashMessage && (
-            <FlashMessage
-              message={flashMessage.message}
-              type={flashMessage.type}
-              onDismiss={() => DismissFlashMessage(showFlashMessage)}
-            />
-          )}
-          <Container
-            fluid
-            className="d-flex justify-content-center align-items-center home-container"
-          >
-            <AppRoutes />
-          </Container>
-        </UserContextProvider>
-      </div>
+    <div className="App">
+      <UserContextProvider>
+        <MainNavBar user={currentUser} />
+        {flashMessage && (
+          <FlashMessage
+            message={flashMessage.message}
+            type={flashMessage.type}
+            onDismiss={() => DismissFlashMessage(showFlashMessage)}
+          />
+        )}
+        <div className="hero-wrapper bg-composed-wrapper bg-midnight-bloom min-vh-100">
+          <div className=" min-vw-50 min-vh-50 w-100 d-flex align-items-center">
+            <Container
+              fluid
+              className=" d-flex justify-content-center align-items-center mb-5"
+            >
+              <AppRoutes />
+            </Container>
+          </div>
+        </div>
+      </UserContextProvider>
+    </div>
   );
 }
 
